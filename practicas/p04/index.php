50d69e4200c561def476d22238e5193fa782b28b
Michel <michel0810.ma@gmail.com>
2023-09-16 21:43:16 -0600

    Ejercicio 3 agregado

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -10,7 +10,7 @@
 <body>
     <h2> Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
-    <form action="http://localhost/tecweb/practicas/p04/index.php" method="get">
+    <form action="http://localhost/tecweb_new/practicas/p04/index.php" method="get">
         <label for="num">Número: </label><br>
         <input type="number" name="numero">
         <input type="submit">
@@ -31,15 +31,15 @@
     <p>Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una
         secuencia compuesta por: impar, par, impar</p>
     <?php
-    $band= true;
+    $band = true;
     $iteracion = 0;
     $numgenerados = 0;
     do {
-        $num1 = rand(1,9999);
-        $num2 = rand(1,9999);
-        $num3 = rand(1,9999);
+        $num1 = rand(1, 9999);
+        $num2 = rand(1, 9999);
+        $num3 = rand(1, 9999);
         $iteracion++;
-        $numgenerados +=3;
+        $numgenerados += 3;
         $matriz[] = [$num1, $num2, $num3];
         if ($num1 % 2 != 0 && $num2 % 2 == 0 && $num3 % 2 != 0) {
             $band = false;
@@ -49,7 +49,7 @@
     //Recorre todo el arreglo 
     foreach ($matriz as $fila) {
         //imprime el contenido de la matriz por fila y separa cada elemento con un espacio
-        echo implode(" ", $fila) . "<br>";
+        echo implode(", ", $fila) . "<br>";
     };
 
     echo "<p>Numero de iteraciones: $iteracion</p>";
@@ -57,6 +57,31 @@
 
     ?>
 
+    <h2>Ejercicio 3</h2>
+    <p>Utiliza un ciclo while para encontrar el primer número entero obtenido aleatoriamente,
+        pero que además sea múltiplo de un número dado.</p>
+    <form action="http://localhost/tecweb_new/practicas/p04/index.php" method="get">
+        <label for="num">Dame un número: </label>
+        <input type="number" name="numeroDado">
+        <input type="submit">
+    </form>
+
+    <?php
+    if (isset($_GET['numeroDado'])) {
+        $numeroDado = $_GET['numeroDado'];
+        $numeroAleatorio = rand(1,999);
+
+        while($numeroAleatorio%$numeroDado !== 0){
+            $numerosGenerados[] = $numeroAleatorio;
+            $numeroAleatorio = rand(1,999);
+        }
+        $numerosGenerados[]=$numeroAleatorio;
+
+        echo "El primer número entero aleatorio que es múltiplo de $numeroDado es: $numeroAleatorio <br>";
+        echo "Los numeros generados aleatoriamnete fueron: <br> ";
+        echo implode(", ", $numerosGenerados);
+    }
+    ?>
 </body>
 
 </html>
